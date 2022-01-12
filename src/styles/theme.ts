1dc094661f59fbaabbe8f6098ee23d8adf92f3be
Murilo Vargas <muriraubach@gmail.com>
2022-01-12 00:54:55 -0300

    chore: add color palette to styled-components theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,28 @@
+const theme = {
+  colors: {
+    black: '#000000',
+    white: '#FFFFFF',
+    gray50: '#EEEEF2',
+    gray100: '#D1D2DC',
+    gray200: '#B3B5C6',
+    gray300: '#9699B0',
+    gray400: '#797D9A',
+    gray500: '#616480',
+    gray600: '#4B4D63',
+    gray700: '#353646',
+    gray800: '#1F2029',
+    gray900: '#181B23',
+    purple50: '#FAF5FF',
+    purple100: '#E9D8FD',
+    purple200: '#D6BCFA',
+    purple300: '#B794F4',
+    purple400: '#9F7AEA',
+    purple500: '#805AD5',
+    purple600: '#6B46C1',
+    purple700: '#553C9A',
+    purple800: '#44337A',
+    purple900: '#322659',
+  }
+}
+
+export default theme
