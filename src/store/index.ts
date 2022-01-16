cae65b261092bd5a33ff0f32e80541307daaaa04
Murilo Vargas <muriraubach@gmail.com>
2022-01-16 16:00:17 -0300

    feat: add middleware in redux store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,8 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './modules/rootReducer'
+import rootSaga from './modules/rootSaga'
 
 import { IPlaylistState } from './modules/favorites/types'
 
@@ -8,6 +10,12 @@ export interface IState {
   favorites: IPlaylistState
 }
 
-const store = createStore(rootReducer)
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewares = [sagaMiddleware]
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+sagaMiddleware.run(rootSaga)
 
 export default store
