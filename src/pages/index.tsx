d5489f5244dc9146703e6b510a180cf30356c6f1
Murilo Vargas <muriraubach@gmail.com>
2022-01-16 16:08:08 -0300

    feat: add request to deezer api from server side on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import Head from 'next/head'
+import { GetStaticProps } from 'next'
 
+import api from '../services/api'
 import SideBar from '../components/SideBar'
+import CardsSection from '../components/CardsSection'
 
 import { Container } from '../styles/pages/Home'
+import { IPlaylistState } from '../store/modules/favorites/types'
 
+interface HomeProps {
+  playlist: IPlaylistState;
+}
 
-export default function Home() {
+export default function Home({ playlist }: HomeProps) {
   return (
     <>
       <Head>
@@ -13,7 +20,34 @@ export default function Home() {
       </Head>
       <Container>
         <SideBar />
+        <CardsSection tracks={playlist.tracks}/>
       </Container>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await api.get('https://api.deezer.com/playlist/3155776842').then(response => response.data)
+  const tracks = response.tracks.data.map(track => {
+    return {
+      id: track.id,
+      title: track.title_short,
+      duration: track.duration,
+      preview: track.preview,
+      artist: track.artist.name,
+      album: track.album.cover,
+      link: track.link,
+      favorite: false,
+    }
+  })
+
+  const playlist = {
+    tracks,
+  }
+
+  return {
+    props: {
+      playlist
+    }
+  }
 }
