1611bb665dcd1bedd63e397158db37cbd517129f
Murilo Vargas <muriraubach@gmail.com>
2022-01-17 20:34:12 -0300

    feat: add remove track from database favorites saga

--- src/store/modules/favorites/sagas.ts
+++ src/store/modules/favorites/sagas.ts
@@ -1,9 +1,10 @@
 import { all, takeLatest,call, put } from 'redux-saga/effects'
 
-import { setFavoritesTracksSuccess, addTrackToFavoritesFailure, addTrackToFavoritesRequest, addTrackToFavoritesSuccess } from './actions'
+import { setFavoritesTracksSuccess, addTrackToFavoritesFailure, addTrackToFavoritesRequest, addTrackToFavoritesSuccess, removeTrackFromFavoritesRequest, removeTrackFromFavoritesSuccess, removeTrackFromFavoritesFailure } from './actions'
 import api from '../../../services/api'
 
 type AddTrackToDatabaseRequest = ReturnType<typeof addTrackToFavoritesRequest>
+type RemoveTrackFromDatabaseRequest = ReturnType<typeof removeTrackFromFavoritesRequest>
 
 function* setFavoritesTracksFromDatabase(action) {
   const { data } = yield call(api.get, '/api/favorites')
@@ -14,7 +15,6 @@ function* addTrackToDatabase({ payload }: AddTrackToDatabaseRequest) {
   const { track } = payload
   const response = yield call(api.post, '/api/favorites', { track: { ...track, favorite: true}})
   const status = response.status
-  console.log(`${status} código`)
 
   if (status === 201) {
     yield put(addTrackToFavoritesSuccess(track))
@@ -25,7 +25,22 @@ function* addTrackToDatabase({ payload }: AddTrackToDatabaseRequest) {
   }
 }
 
+function* removeTrackFromDatabase({ payload }: RemoveTrackFromDatabaseRequest) {
+  const { track } = payload
+  const response = yield call(api.delete, '/api/favorites', { data: { trackId: track.id } })
+  const status = response.status
+
+  if (status === 201) {
+    yield put(removeTrackFromFavoritesSuccess(track))
+  }
+
+  if (status === 501) {
+    yield put(removeTrackFromFavoritesFailure())
+  }
+}
+
 export default all([
+  takeLatest('REMOVE_TRACK_FROM_FAVORITES_REQUEST', removeTrackFromDatabase),
   takeLatest('SET_FAVORITES_TRACKS_REQUEST', setFavoritesTracksFromDatabase),
   takeLatest('ADD_TRACK_TO_FAVORITES_REQUEST', addTrackToDatabase)
 ])
