2a3835d2519c253d07a03aaff26083390b6aa6c0
Murilo Vargas <muriraubach@gmail.com>
2022-01-17 20:18:08 -0300

    refactor: add action types in types file

--- src/store/modules/favorites/types.ts
+++ src/store/modules/favorites/types.ts
@@ -1,3 +1,14 @@
+export enum ActionTypes {
+  setFavoritesTracksRequest = 'SET_FAVORITES_TRACKS_REQUEST',
+  setFavoritesTracksSuccess = 'SET_FAVORITES_TRACKS_SUCCESS',
+  addTrackToFavoritesRequest = 'ADD_TRACK_TO_FAVORITES_REQUEST',
+  addTrackToFavoritesSuccess = 'ADD_TRACK_TO_FAVORITES_SUCCESS',
+  addTrackToFavoritesFailure = 'ADD_TRACK_TO_FAVORITES_FAILURE',
+  removeTrackFromFavoritesRequest = 'REMOVE_TRACK_FROM_FAVORITES_REQUEST',
+  removeTrackFromFavoritesSuccess = 'REMOVE_TRACK_FROM_FAVORITES_SUCCESS',
+  removeTrackFromFavoritesFailure = 'REMOVE_TRACK_FROM_FAVORITES_FAILURE',
+}
+
 export interface ITrack {
   id: number
   title: string
