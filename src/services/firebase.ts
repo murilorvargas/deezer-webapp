c07e0be6e9edecf1fba09e3532aec0cd3561a0cb
Murilo Vargas <muriraubach@gmail.com>
2022-01-18 20:41:15 -0300

    fix: change database url from firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,7 +5,7 @@ import { getDatabase } from 'firebase/database'
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  databaseURL: "https://web-app-deezer-default-rtdb.firebaseio.com",
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_I,
