e3e08e47a3177cf7bf06a884f04aee2498005320
Murilo Vargas <muriraubach@gmail.com>
2022-01-22 23:52:13 -0300

    refactor: remove method get from favorites route in next api

--- src/pages/api/favorites.ts
+++ src/pages/api/favorites.ts
@@ -2,20 +2,9 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { parseCookies, setCookie } from 'nookies'
 import { v4 as uuidV4 } from 'uuid'
 import { database } from '../../services/firebase';
-import { set, ref, child, get, remove } from 'firebase/database';
+import { set, ref, remove } from 'firebase/database';
 
-type FirebaseResponse = Record<string, {
-  id: number
-  title: string
-  duration: number
-  preview: string
-  artist: string
-  album: string
-  link: string
-  favorite: boolean
-}>
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   if (req.method === 'POST') {
     const { track } = req.body;
     const { ['web-app_deezer.uuid']: userUuid } = parseCookies({ req })
@@ -51,37 +40,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     }
   }
 
-  if (req.method === 'GET') {
-    const { ['web-app_deezer.uuid']: userUuid } = parseCookies({ req })
-    const dbRef = ref(database)
-
-    return get(child(dbRef, `users/${userUuid}/tracks`))
-    .then(snapshot => {
-        const response: FirebaseResponse = snapshot.val() ?? {}
-        const data = Object.entries(response).map(([key, value]) => {
-        return {
-          album: value.album,
-          artist: value.artist,
-          duration: value.duration,
-          favorite: value.favorite,
-          id: Number(key),
-          link: value.link,
-          preview: value.preview,
-          title: value.title,
-        }
-      })
-      return res.json(data);
-    })
-    .catch(err => {
-      return res.status(400).json(err);
-    });
-  }
-
   if(req.method === 'DELETE') {
     const { trackId } = req.body;
     const { ['web-app_deezer.uuid']: userUuid } = parseCookies({ req })
 
-    remove(ref(database, `users/${userUuid}/tracks/${trackId}`))
+    return remove(ref(database, `users/${userUuid}/tracks/${trackId}`))
     .then(() => {
       return res.status(201).json({ success: true });
     })
