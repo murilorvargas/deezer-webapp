346ada9edfde2f55e1f8795403cae4351d996c45
Murilo Vargas <muriraubach@gmail.com>
2022-01-22 23:33:00 -0300

    feat: add better error handling in favorites middlewares

--- src/store/modules/favorites/sagas.ts
+++ src/store/modules/favorites/sagas.ts
@@ -8,7 +8,8 @@ import {
   removeTrackFromFavoritesRequest,
   removeTrackFromFavoritesSuccess,
   removeTrackFromFavoritesFailure, 
-  setFavoritesTracksRequest} from './actions'
+  setFavoritesTracksRequest,
+  setFavoritesTracksFailure} from './actions'
 import api from '../../../services/api'
 import { getDataFromDatabase } from '../../../utils/getDataFromDatabase';
 
@@ -20,36 +21,35 @@ type AddTrackToDatabaseRequest = ReturnType<typeof addTrackToFavoritesRequest>
 type RemoveTrackFromDatabaseRequest = ReturnType<typeof removeTrackFromFavoritesRequest>
 
 function* setFavoritesTracksFromDatabase({ payload }: SetFavoritesTracksRequest) {
-  const data = yield getDataFromDatabase(payload.ctx)
-  yield put(setFavoritesTracksSuccess(data))
+  try {
+    const data = yield getDataFromDatabase(payload.ctx)
+    yield put(setFavoritesTracksSuccess(data))
+  } catch (error) {
+    console.log(error)
+    yield put(setFavoritesTracksFailure())
+  }
 }
 
 function* addTrackToDatabase({ payload }: AddTrackToDatabaseRequest) {
-  const { track } = payload
-  const response = yield call(api.post, '/api/favorites', { track: { ...track, favorite: true}})
-  const status = response.status
-
-  if (status === 201) {
+  try {
+    const { track } = payload
+    yield call(api.post, '/api/favorites', { track: { ...track, favorite: true}})
     yield put(handleDeezerFavoritesTracks(track.id))
     yield put(addTrackToFavoritesSuccess(track))
-  }
-
-  if (status === 501) {
+  } catch (error) {
+    console.log(error)
     yield put(addTrackToFavoritesFailure())
   }
 }
 
 function* removeTrackFromDatabase({ payload }: RemoveTrackFromDatabaseRequest) {
-  const { track } = payload
-  const response = yield call(api.delete, '/api/favorites', { data: { trackId: track.id } })
-  const status = response.status
-
-  if (status === 201) {
+  try {
+    const { track } = payload
+    yield call(api.delete, '/api/favorites', { data: { trackId: track.id } })
     yield put(handleDeezerFavoritesTracks(track.id))
     yield put(removeTrackFromFavoritesSuccess(track))
-  }
-
-  if (status === 501) {
+  } catch (error) {
+    console.log(error)
     yield put(removeTrackFromFavoritesFailure())
   }
 }
