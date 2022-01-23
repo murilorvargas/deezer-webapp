d5ea573b12280a92a53002e13523a66d807c8f11
Murilo Vargas <muriraubach@gmail.com>
2022-01-22 22:56:27 -0300

    feat: add deezer tracks redux module in root reducer

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import favorites from './favorites/reducer'
+import deezerTracks from './deezerTracks/reducer'
 
 export default combineReducers({
   favorites,
+  deezerTracks,
 })
